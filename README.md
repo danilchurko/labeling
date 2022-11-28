a308fcae19600a49183fe6e173a6d5f6d3786044
Danil Churko <danil.churko@gmail.com>
2022-11-28 00:05:44 +0000

    work v1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # labeling
+
+Instruction 4 use
+
+Linux
+
+MacOS
+- install brew (run command in terminal)
+- /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+---
+- brew install pyenv
+- pyenv install 3.9.2
+- python3 -m pip install --upgrade pip
+- pip3 install webdriver-manager
+- pip3 install pandas
+
---
+++ google_labeling_chrome.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import pandas as pd
+import re
+import time
+import random
+
+mem_index = 0
+limit = 5
+
+df = pd.read_csv("book.csv")  # file
+df = df.iloc[:, 4] + ' ' + df.iloc[:, 7]  # columns
+
+fin_list = []  # final list with search row
+
+
+#  List without dupes
+def unique_list(dirt_list):
+    ulist = []
+    [ulist.append(x) for x in dirt_list if x not in ulist]
+    return ulist
+
+
+#  Creating list without spaces
+def create_list():
+    for row in df:
+        result = re.sub(r'\s+', ' ', row)
+        result2 = ' '.join(unique_list(result.split()))
+        fin_list.append(result2)
+
+
+def launch_browser(fin_list, mem_index):
+    index = 0
+
+    chrome_options = Options()
+    chrome_options.add_argument("--incognito, detach")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    for itm in fin_list[mem_index:]:
+        index += 1
+
+        i_ns = itm.replace(" ", "+")
+        driver.get('https://www.google.com/search?q=' + i_ns + '&gl=us&hl=en&pws=0&gws_rd=cr')
+
+        if itm == fin_list[0]:
+            time.sleep(5)
+        else:
+            time.sleep(random.randint(0, 2))
+
+        driver.switch_to.new_window('tab')
+
+        mem_index += 1
+
+        if index == limit:
+            break
+
+    print(mem_index)
+    return driver, mem_index
+
+
+def start():
+    drv, memi = launch_browser(fin_list, mem_index)
+
+    if input('>>> ') == 'n':
+        time.sleep(1)
+
+    return drv, memi
+
+
+create_list()
+
+for run in range(len(fin_list)):
+    driver, mem_index = start()
